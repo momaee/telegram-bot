90d333a3a68b976361e8a73d14b9913bbae528db
Mohammad <drmo.ma@yahoo.com>
2023-04-16 21:47:35 +0200

    feat: add health check
    Signed-off-by: Mohammad Mahmoodian <drmo.ma@yahoo.com>

--- main.go
+++ main.go
@@ -1,37 +1,33 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// [START gae_go111_app]
-
-// Sample helloworld is an App Engine app.
 package main
 
-// [START import]
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
+	"hash/crc32"
 	"log"
 	"net/http"
 	"os"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-// [END import]
-// [START main_func]
+var (
+	telegramAPIKey = "projects/" + os.Getenv("SECRETS_PROJECT_ID") + "/secrets/TELEGRAM_API_KEY/versions/latest"
+	openaiAPIKey   = "projects/" + os.Getenv("SECRETS_PROJECT_ID") + "/secrets/OPENAI_API_KEY/versions/latest"
+)
 
 func main() {
-	http.HandleFunc("/", indexHandler)
+	go bot()
 
-	// [START setting_port]
+	server()
+}
+
+func server() {
+	http.HandleFunc("/health", indexHandler)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -42,21 +38,192 @@ func main() {
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
-	// [END setting_port]
 }
 
-// [END main_func]
-
-// [START indexHandler]
-
-// indexHandler responds to requests with our greeting.
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
+	if r.URL.Path != "/health" {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprint(w, "Hello, World!")
+	fmt.Fprint(w, "Healthy")
 }
 
-// [END indexHandler]
-// [END gae_go111_app]
+func bot() {
+	// Create the client.
+	ctx := context.Background()
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		log.Printf("failed to create secretmanager client: %v", err)
+		return
+	}
+	defer client.Close()
+
+	log.Printf("telegramAPIKey: %s", telegramAPIKey)
+	// Build the request.
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: telegramAPIKey,
+	}
+
+	// Call the API.
+	result, err := client.AccessSecretVersion(ctx, req)
+	if err != nil {
+		log.Printf("failed to access secret version: %v", err)
+		return
+	}
+
+	// Verify the data checksum.
+	crc32c := crc32.MakeTable(crc32.Castagnoli)
+	checksum := int64(crc32.Checksum(result.Payload.Data, crc32c))
+	if checksum != *result.Payload.DataCrc32C {
+		log.Printf("Data corruption detected.")
+		return
+	}
+
+	telegramAPIKey = string(result.Payload.Data)
+
+	log.Printf("openaiAPIKey: %s", openaiAPIKey)
+	// Build the request.
+	req = &secretmanagerpb.AccessSecretVersionRequest{
+		Name: openaiAPIKey,
+	}
+
+	// Call the API.
+	result, err = client.AccessSecretVersion(ctx, req)
+	if err != nil {
+		log.Printf("failed to access secret version: %v", err)
+		return
+	}
+
+	// Verify the data checksum.
+	crc32c = crc32.MakeTable(crc32.Castagnoli)
+	checksum = int64(crc32.Checksum(result.Payload.Data, crc32c))
+	if checksum != *result.Payload.DataCrc32C {
+		log.Printf("Data corruption detected.")
+		return
+	}
+
+	openaiAPIKey = string(result.Payload.Data)
+
+	botLoop()
+}
+
+func botLoop() {
+	bot, err := tgbotapi.NewBotAPI(telegramAPIKey)
+	if err != nil {
+		log.Println("error creating bot:", err)
+		return
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+
+			// If the message is reply to someone else, ignore it.
+			if update.Message.ReplyToMessage != nil && update.Message.ReplyToMessage.From != nil {
+				if update.Message.ReplyToMessage.From.UserName != bot.Self.UserName {
+					continue
+				}
+			}
+
+			response, err := chatGPT(update.Message.Text)
+			if err != nil {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, err.Error())
+				msg.ReplyToMessageID = update.Message.MessageID
+				if _, err := bot.Send(msg); err != nil {
+					log.Println("error sending message:", err)
+				}
+				continue
+			}
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, response)
+			msg.ReplyToMessageID = update.Message.MessageID
+			if _, err := bot.Send(msg); err != nil {
+				log.Println("error sending message:", err)
+			}
+		}
+	}
+}
+
+func chatGPT(content string) (string, error) {
+	type Messages struct {
+		Role    string `json:"role"`
+		Content string `json:"content"`
+	}
+
+	type Payload struct {
+		Model    string     `json:"model"`
+		Messages []Messages `json:"messages"`
+	}
+
+	data := Payload{
+		Model: "gpt-3.5-turbo",
+		Messages: []Messages{
+			{
+				Role:    "user",
+				Content: content,
+			},
+		},
+	}
+	payloadBytes, err := json.Marshal(data)
+	if err != nil {
+		return "", err
+	}
+	body := bytes.NewReader(payloadBytes)
+
+	req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", body)
+	if err != nil {
+		fmt.Println("error:", err)
+		return "", err
+	}
+	req.Header.Set("Authorization", "Bearer "+openaiAPIKey)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println("error:", err)
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("error:", resp.Status)
+		return "", err
+	}
+
+	type Response struct {
+		ID      string `json:"id"`
+		Object  string `json:"object"`
+		Created int    `json:"created"`
+		Model   string `json:"model"`
+		Usage   struct {
+			PromptTokens     int `json:"prompt_tokens"`
+			CompletionTokens int `json:"completion_tokens"`
+			TotalTokens      int `json:"total_tokens"`
+		} `json:"usage"`
+		Choices []struct {
+			Message struct {
+				Role    string `json:"role"`
+				Content string `json:"content"`
+			} `json:"message"`
+			FinishReason string `json:"finish_reason"`
+			Index        int    `json:"index"`
+		} `json:"choices"`
+	}
+
+	var response Response
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		fmt.Println("error:", err)
+		return "", err
+	}
+
+	return response.Choices[0].Message.Content, nil
+}
