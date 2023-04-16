58b1fd0a46e58354ced147bd3411956ee149f5a3
Mohammad <drmo.ma@yahoo.com>
2023-04-16 21:35:43 +0200

    deploy to google app engine

--- main.go
+++ main.go
@@ -1,172 +1,62 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START gae_go111_app]
+
+// Sample helloworld is an App Engine app.
 package main
 
+// [START import]
 import (
-	"bufio"
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strings"
-
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+// [END import]
+// [START main_func]
+
 func main() {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_API_KEY"))
-	if err != nil {
-		fmt.Println("error new bot api:", err)
+	http.HandleFunc("/", indexHandler)
+
+	// [START setting_port]
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+	// [END setting_port]
+}
+
+// [END main_func]
+
+// [START indexHandler]
+
+// indexHandler responds to requests with our greeting.
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
 		return
 	}
-
-	bot.Debug = true
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message != nil { // If we got a message
-
-			if update.Message.From.UserName == "sirAlif" {
-				if update.Message.From.LanguageCode == "en" {
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Come eat it's head")
-					msg.ReplyToMessageID = update.Message.MessageID
-					bot.Send(msg)
-				} else {
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "بیا سرشو بخور")
-					msg.ReplyToMessageID = update.Message.MessageID
-					bot.Send(msg)
-				}
-				continue
-			}
-
-			// If the message is reply to someone else, ignore it.
-			if update.Message.ReplyToMessage != nil && update.Message.ReplyToMessage.From != nil {
-				if update.Message.ReplyToMessage.From.UserName != bot.Self.UserName {
-					continue
-				}
-			}
-
-			response, err := call(update.Message.Text)
-			if err != nil {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, err.Error())
-				msg.ReplyToMessageID = update.Message.MessageID
-				bot.Send(msg)
-				continue
-			}
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, response)
-			msg.ReplyToMessageID = update.Message.MessageID
-			bot.Send(msg)
-		}
-	}
+	fmt.Fprint(w, "Hello, World!")
 }
 
-func loop() {
-	for {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Println("Enter your question:")
-		input, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("error:", err)
-			return
-		}
-		input = strings.Replace(input, "\n", "", -1)
-
-		if input == "exit" {
-			break
-		}
-
-		output, err := call(input)
-		if err != nil {
-			fmt.Println("error:", err)
-			return
-		}
-
-		fmt.Println("output:", output)
-	}
-}
-
-func call(content string) (string, error) {
-	// Set your OpenAI API key as an environment variable.
-	apiKey := os.Getenv("OPENAI_API_KEY")
-
-	type Messages struct {
-		Role    string `json:"role"`
-		Content string `json:"content"`
-	}
-
-	type Payload struct {
-		Model    string     `json:"model"`
-		Messages []Messages `json:"messages"`
-	}
-
-	data := Payload{
-		Model: "gpt-3.5-turbo",
-		Messages: []Messages{
-			{
-				Role:    "user",
-				Content: content,
-			},
-		},
-	}
-	payloadBytes, err := json.Marshal(data)
-	if err != nil {
-		return "", err
-	}
-	body := bytes.NewReader(payloadBytes)
-
-	req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", body)
-	if err != nil {
-		fmt.Println("error:", err)
-		return "", err
-	}
-	req.Header.Set("Authorization", "Bearer "+apiKey)
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		fmt.Println("error:", err)
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("error:", resp.Status)
-		return "", err
-	}
-
-	type Response struct {
-		ID      string `json:"id"`
-		Object  string `json:"object"`
-		Created int    `json:"created"`
-		Model   string `json:"model"`
-		Usage   struct {
-			PromptTokens     int `json:"prompt_tokens"`
-			CompletionTokens int `json:"completion_tokens"`
-			TotalTokens      int `json:"total_tokens"`
-		} `json:"usage"`
-		Choices []struct {
-			Message struct {
-				Role    string `json:"role"`
-				Content string `json:"content"`
-			} `json:"message"`
-			FinishReason string `json:"finish_reason"`
-			Index        int    `json:"index"`
-		} `json:"choices"`
-	}
-
-	var response Response
-	err = json.NewDecoder(resp.Body).Decode(&response)
-	if err != nil {
-		fmt.Println("error:", err)
-		return "", err
-	}
-
-	return response.Choices[0].Message.Content, nil
-}
+// [END indexHandler]
+// [END gae_go111_app]
